b7e9e44ae35c6a42a0cec2df7bda85b0dd92975f
carlosmarinr <carlosmarin2511@gmail.com>
2026-05-30 17:07:52 -0400

    update is done

--- newspapers/views.py
+++ newspapers/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Blog
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (ListView, 
+                                  DetailView, 
+                                  CreateView, 
+                                  UpdateView,)
 
 # Create your views here.
 class BlogListView(ListView):
@@ -15,3 +18,8 @@ class BlogCreateView(CreateView):
     model = Blog
     template_name = 'post-create.html'
     fields = ['title', 'content', 'author']
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    template_name = 'post-update.html'
+    fields = ['title', 'content']
